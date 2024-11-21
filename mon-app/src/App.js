fc461843197819cb4bedcd1b5b0aab1e5eeb2729
malek <malekkhefifi5@gmail.com>
2024-11-21 09:44:27 +0100

    first commit

---
+++ mon-app/src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import './App.css'; // Importer le fichier CSS
+import UserList from './UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React User List Example</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
+
+
+
